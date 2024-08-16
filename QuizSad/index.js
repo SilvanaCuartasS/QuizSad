d6ecc438a560d30d4504f5492a2000e884bde157
SilvanaCuartas <165075107+SilvanaCuartasS@users.noreply.github.com>
2024-08-16 17:44:25 -0500

    Add files via upload

---
+++ QuizSad/index.js
@@ -0,0 +1,61 @@
+//Ejercicio 1 
+
+const objets = [
+    {
+        id: 1,
+        name: "matemáticas",
+        completed: false
+    },
+    {
+        id: 2,
+        name: "matemáticas",
+        completed: false
+    },
+    {
+        id: 3,
+        name: "matemáticas",
+        completed: false
+    }
+];
+
+const homeWork = (id, completed) => {
+    id = 0
+    completed = false
+    objets.forEach((id, completed) => {
+    if (objets.completed = false) {
+        return true;
+    };
+});
+
+document.getElementById(console.log(homeWork.id));
+};
+
+
+//Ejercicio 2
+
+const objets2 = [
+    {
+        name: "Colombia",
+        poblation: 1000000
+    },
+    {
+        name: "Chile",
+        poblation: 2000000
+    },
+    {
+        name: "China",
+        poblation: 100000000
+    }
+];
+
+
+const nameCountry = []
+const maximum = (nameCountry) => {
+    nameCountry = poblation
+    objets2.forEach((nameCountry) => {
+    if (objets2.poblation > 10000000 ) {
+        return poblation;
+    };
+});
+console.log(maximum);
+};
